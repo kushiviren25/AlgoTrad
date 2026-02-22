e3874e170816d388eb8411530d522467c8a51ff0
Kushi Virendran <kushivirendran25@gmail.com>
2026-02-22 21:56:22 +0530

    Initial Commit

---
+++ data_clean.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv("C:/Users/admin/Downloads/AlgoTrad/trad_data.csv")
+
+print(df.describe())
---
+++ data_gen.py
@@ -0,0 +1,45 @@
+import requests 
+import pandas as pd 
+
+
+API_KEY =  "REMOVED_API_KEY"
+SYMBOL = "AAPL"
+
+url = "https://www.alphavantage.co/query"
+
+params = {
+    "function" : "TIME_SERIES_DAILY",
+    "apikey" : API_KEY,
+    "symbol"  : SYMBOL,         # docx based params 
+    "outputsize" : "compact"
+}
+
+response = requests.get(url,params=params)
+
+# pull out the data :
+data = response.json()
+
+# check if the data is properly pulled :
+print(data.keys())
+
+ts = data["Time Series (Daily)"] # -> Documentation based 
+
+# Convert the api response into dataframes (currently is a dict -> dataframe)
+df = pd.DataFrame.from_dict(ts, orient ="index")
+
+# VALID DATA CONVERSIONS :
+
+# Convert to date and time
+df.index = pd.to_datetime(df.index)
+
+# Convert strings -> float:
+df = df.astype(float)
+
+# Define the columns 
+df.columns = ["open", "high", "low", "close", "volume"]
+
+# Convert to csv to save from api limit issues 
+df.to_csv(r"C:/Users/admin/Downloads/trad_data.csv")
+
+print("Data sucessfully fetched from API and saved to csv")
+
